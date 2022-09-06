880224c9aeaf73d947cff59175252630b2d00159
Serhan Ertürk <srnerturk@gmail.com>
2022-09-06 12:15:19 +0300

    test

--- cypress/e2e/visitFb.cy.js
+++ cypress/e2e/visitFb.cy.js
@@ -1,9 +1,9 @@
 context("Visit Meta231", () => {
-    beforeEach(() => {
-        cy.visit("https://www.facebook.com");
-    });
-    it("Sendkeys username", () => {
-        cy.get('[data-testid="royal_email"]').type("123456");
-        cy.get('[data-testid="royal_email"]').clear();
-});
+  beforeEach(() => {
+    cy.visit("https://www.facebook.com");
+  });
+  it("Sendkeys username", () => {
+    cy.get('[data-testid="royal_email"]').type("123456");
+    cy.get('[data-testid="royal_email"]').clear();
+  });
 });
