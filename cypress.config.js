af8353a41c9986f296ad2b4be63ef443b30652c6
Serhan Ertürk <srnerturk@gmail.com>
2022-09-06 12:04:59 +0300

    changed project id

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 't143h8',
+  projectId: 'tmvcfp',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
